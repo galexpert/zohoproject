9c91a8eaa50e536aef19e3740cbf1126553d374d
Vania <galexpert@ukr.net>
2024-05-14 22:52:28 +0200

    Initial commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+    11111111111111
+</template>
+
+<script>
+   // import TestComponent from "./TestComponent.vue";
+    export default {
+        name: "App",
+
+        components: {
+            /*TestComponent: TestComponent,*/
+          /*  TestComponent*/
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+
+    }
+
+</script>
+
+<style scoped>
+
+</style>
